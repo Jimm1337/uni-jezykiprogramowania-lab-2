2c9d0c46038ddc9d87fb21236e82e8e145090123
oskar <263970@student.pwr.edu.pl>
2022-11-06 19:07:02 +0100

    [Refactor] Refactor: Make code more verbose, remove bad comments.

--- src/tb/soft/PersonApp.java
+++ src/tb/soft/PersonApp.java
@@ -10,49 +10,45 @@ import java.util.Arrays;
  * Autor: Paweł Rogaliński
  * Data: październik 2018 r.
  */
+
+/**
+ * 06.11.2022
+ * Autor rozwiązania i modyfikacji: Oskar Gusta 263970
+ */
+
 public class PersonApp {
 
     private static final String GREETING_MESSAGE =
-        """
-        Program Person - wersja konsolowa
-        Autor: Paweł Rogaliński
-        Data:  październik 2018 r.
-        """;
+            """
+                    Program PersonApp
+                    Autor: Oskar Gusta
+                    Data:  06.11.2022 r.
+                    """;
 
 
     private static final String MENU =
-        """
-        M E N U   G Ł Ó W N E
-        1 - Podaj dane nowej osoby
-        2 - Usuń dane osoby   
-        3 - Modyfikuj dane osoby
-        4 - Wczytaj dane z pliku
-        5 - Zapisz dane do pliku
-        0 - Zakończ program
-        """;
+            """
+                    M E N U   G Ł Ó W N E
+                    1 - Podaj dane nowej osoby
+                    2 - Usuń dane osoby   
+                    3 - Modyfikuj dane osoby
+                    4 - Wczytaj dane z pliku
+                    5 - Zapisz dane do pliku
+                    0 - Zakończ program
+                    """;
 
     private static final String CHANGE_MENU =
-        """
-        Co zmienić?
-        1 - Imię
-        2 - Nazwisko
-        3 - Rok urodzenia
-        4 - Stanowisko
-        0 - Powrót do menu głównego
-        """;
+            """
+                    Co zmienić?
+                    1 - Imię
+                    2 - Nazwisko
+                    3 - Rok urodzenia
+                    4 - Stanowisko
+                    0 - Powrót do menu głównego
+                    """;
 
-
-    /**
-     * UserDialogConsole to pomocnicza klasa zawierająca zestaw
-     * prostych metod do realizacji dialogu z użytkownikiem
-     * w oknie konsoli tekstowej.
-     */
     private static final IUserDialog UI = new UserDialogJOption();
 
-
-    /*
-     *  Referencja do obiektu, który zawiera dane aktualnej osoby.
-     */
     private Person currentPerson = null;
 
 
@@ -67,48 +63,19 @@ public class PersonApp {
 
         while (true) {
             UI.clearConsole();
-            showCurrentPerson();
+            currentPersonShow();
 
             try {
                 switch (UI.enterInt(MENU + "==>> ")) {
-                    case 1:
-                        // utworzenie nowej osoby
-                        currentPerson = createNewPerson();
-                        break;
-                    case 2:
-                        // usunięcie danych aktualnej osoby.
-                        currentPerson = null;
-                        UI.printInfoMessage("Dane aktualnej osoby zostały usunięte");
-                        break;
-                    case 3:
-                        // zmiana danych dla aktualnej osoby
-                        if (currentPerson == null) throw new PersonException("Żadna osoba nie została utworzona.");
-                        changePersonData(currentPerson);
-                        break;
-                    case 4: {
-                        // odczyt danych z pliku tekstowego.
-                        String file_name = UI.enterString("Podaj nazwę pliku: ");
-                        currentPerson = Person.readFromFile(file_name);
-                        UI.printInfoMessage("Dane aktualnej osoby zostały wczytane z pliku " + file_name);
-                    }
-                    break;
-                    case 5: {
-                        // zapis danych aktualnej osoby do pliku tekstowego
-                        String file_name = UI.enterString("Podaj nazwę pliku: ");
-                        Person.printToFile(file_name, currentPerson);
-                        UI.printInfoMessage("Dane aktualnej osoby zostały zapisane do pliku " + file_name);
-                    }
-                    break;
-                    case 0:
-                        // zakończenie działania programu
-                        UI.printInfoMessage("\nProgram zakończył działanie!");
-                        System.exit(0);
-                } // koniec instrukcji switch
+                    case 1 -> currentPersonCreate(); // utworzenie nowej osoby
+                    case 2 -> currentPersonDelete(); // usunięcie danych aktualnej osoby.
+                    case 3 -> currentPersonChange(); // zmiana danych dla aktualnej osoby
+                    case 4 -> currentPersonLoadFromFile(); // odczyt danych z pliku tekstowego.
+                    case 5 -> currentPersonSave(); // zapis danych aktualnej osoby do pliku tekstowego
+                    case 0 -> exit(); // zakończenie działania programu
+                    default -> UI.printErrorMessage("Niepoprawny wybór!");
+                }
             } catch (PersonException e) {
-                // Tu są wychwytywane wyjątki zgłaszane przez metody klasy Person,
-                // gdy nie są spełnione ograniczenia nałożone na dopuszczalne wartości
-                // poszczególnych atrybutów.
-                // Drukowanie komunikatu o błędzie zgłoszonym za pomocą wyjątku PersonException.
                 UI.printErrorMessage(e.getMessage());
             }
         } // koniec pętli while
@@ -116,19 +83,10 @@ public class PersonApp {
 
 
     /*
-     *  Metoda wyświetla w oknie konsoli dane aktualnej osoby
-     *  pamiętanej w zmiennej currentPerson.
-     */
-    void showCurrentPerson() {
-        showPerson(currentPerson);
-    }
-
-
-    /*
      * Metoda wyświetla w oknie konsoli dane osoby reprezentowanej
      * przez obiekt klasy Person
      */
-    static void showPerson(Person person) {
+    private static void showPerson(Person person) {
         StringBuilder sb = new StringBuilder();
 
         if (person != null) {
@@ -150,7 +108,7 @@ public class PersonApp {
      * klasy Person. Jeśli zostaną wykryte niepoprawne dane,
      * to zostanie zgłoszony wyjątek, który zawiera komunikat o błędzie.
      */
-    static Person createNewPerson() {
+    private static Person createNewPerson() {
         String first_name = UI.enterString("Podaj imię: ");
         String last_name = UI.enterString("Podaj nazwisko: ");
         String birth_year = UI.enterString("Podaj rok ur.: ");
@@ -158,16 +116,10 @@ public class PersonApp {
         String job_name = UI.enterString("Podaj stanowisko: ");
         Person person;
         try {
-            // Utworzenie nowego obiektu klasy Person oraz
-            // ustawienie wartości wszystkich atrybutów.
             person = new Person(first_name, last_name);
             person.setBirthYear(birth_year);
             person.setJob(job_name);
-        } catch (PersonException e) {
-            // Tu są wychwytywane wyjątki zgłaszane przez metody klasy Person,
-            // gdy nie są spełnione ograniczenia nałożone na dopuszczalne wartości
-            // poszczególnych atrybutów.
-            // Drukowanie komunikatu o błędzie zgłoszonym za pomocą wyjątku PersonException.
+        } catch (PersonException e) { // Wychwycenie błędu gdy nie są spełnione ograniczenia nałożone na dopuszczalne wartości.
             UI.printErrorMessage(e.getMessage());
             return null;
         }
@@ -182,38 +134,66 @@ public class PersonApp {
      * klasy Person. Jeśli zostaną wykryte niepoprawne dane,
      * to zostanie zgłoszony wyjątek, który zawiera komunikat o błędzie.
      */
-    static void changePersonData(Person person) {
+    private static void changePersonData(Person person) {
         while (true) {
             UI.clearConsole();
             showPerson(person);
 
             try {
                 switch (UI.enterInt(CHANGE_MENU + "==>> ")) {
-                    case 1:
-                        person.setFirstName(UI.enterString("Podaj imię: "));
-                        break;
-                    case 2:
-                        person.setLastName(UI.enterString("Podaj nazwisko: "));
-                        break;
-                    case 3:
-                        person.setBirthYear(UI.enterString("Podaj rok ur.: "));
-                        break;
-                    case 4:
-                        UI.printMessage("Dozwolone stanowiska:" + Arrays.deepToString(PersonJob.values()));
-                        person.setJob(UI.enterString("Podaj stanowisko: "));
-                        break;
-                    case 0:
-                        return;
-                }  // koniec instrukcji switch
+                    case 1 -> person.setFirstName(UI.enterString("Podaj imię: "));
+                    case 2 -> person.setLastName(UI.enterString("Podaj nazwisko: "));
+                    case 3 -> person.setBirthYear(UI.enterString("Podaj rok ur.: "));
+                    case 4 -> person.setJob(UI.enterString("Podaj stanowisko: "));
+                    case 0 -> { return; }
+                    default -> UI.printErrorMessage("Niepoprawny wybór!");
+                }
             } catch (PersonException e) {
-                // Tu są wychwytywane wyjątki zgłaszane przez metody klasy Person,
-                // gdy nie są spełnione ograniczenia nałożone na dopuszczalne wartości
-                // poszczególnych atrybutów.
-                // Drukowanie komunikatu o błędzie zgłoszonym za pomocą wyjątku PersonException.
                 UI.printErrorMessage(e.getMessage());
             }
         }
     }
 
+
+    private void currentPersonShow() {
+        showPerson(currentPerson);
+    }
+
+
+    private void currentPersonCreate() {
+        currentPerson = createNewPerson();
+    }
+
+
+    private void currentPersonDelete() {
+        currentPerson = null;
+        UI.printInfoMessage("Dane aktualnej osoby zostały usunięte");
+    }
+
+
+    private void currentPersonChange() throws PersonException {
+        if (currentPerson == null) throw new PersonException("Żadna osoba nie została utworzona.");
+        changePersonData(currentPerson);
+    }
+
+
+    private void currentPersonLoadFromFile() throws PersonException {
+        String file_name = UI.enterString("Podaj nazwę pliku: ");
+        currentPerson = Person.readFromFile(file_name);
+        UI.printInfoMessage("Dane aktualnej osoby zostały wczytane z pliku " + file_name);
+    }
+
+
+    private void currentPersonSave() throws PersonException {
+        String file_name = UI.enterString("Podaj nazwę pliku: ");
+        currentPerson = Person.readFromFile(file_name);
+        UI.printInfoMessage("Dane aktualnej osoby zostały wczytane z pliku " + file_name);
+    }
+
+
+    private void exit() {
+        UI.printInfoMessage("\nProgram zakończył działanie!");
+        System.exit(0);
+    }
 
 }  // koniec klasy PersonApp
